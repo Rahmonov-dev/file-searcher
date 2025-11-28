6ea7df6bbc0f0cf99fda4c5f8660b46227e8ca45
Baxtiyor <baxtiyorr545@gmail.com>
2025-11-29 00:10:14 +0500

    :)

---
+++ src/test/java/org/architect/filereader/FileReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.architect.filereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
